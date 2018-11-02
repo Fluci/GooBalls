c6b9ee0eb0eb43d6de93244b97b994ae5bb6b6c2
Felice Serena <felice@serena-mueller.ch>
2018-11-02 10:29:55 +0100

    Minor fix ups

--- src/lib/physics/2d/engine.cpp
+++ src/lib/physics/2d/engine.cpp
@@ -9,24 +9,25 @@ namespace Physics {
 void Engine::advance(Scene& scene, TimeStep dt) {
     scene.world.Step(dt, velocity_iterations, position_iterations);
     return;
-    // The rigid bodies moved, use their transform to compute the local meshes
+    // The rigid bodies moved: use their transform to compute the local meshes
     for(auto& mesh : scene.meshes){
         Eigen::Matrix<FloatPrecision, 1, 2> dx(1,2);
         dx[0] = mesh.body->GetPosition().x;
         dx[1] = mesh.body->GetPosition().y;
-        // TODO: add rotation
         FloatPrecision radianAngle = mesh.body->GetAngle();
         // eigen convention: counter-clockwise rotation in radians
         Eigen::Rotation2D<FloatPrecision> rot(radianAngle);
         Eigen::Matrix<FloatPrecision, 2, 2> rotM = rot.toRotationMatrix();
         const auto& loc = mesh.vertices_position_local();
         auto& glob = mesh.vertices_position_global();
-        // it is not clear, if rotM needs to be transposed here -> test
+        // TODO: it is not clear, if rotM needs to be transposed here -> test
         rotM.transpose();
         glob = loc * rotM;
         glob = glob.rowwise() + dx;
     }
-    scene.fluid->particles_position() += scene.fluid->particles_velocity()*dt;
+    if(scene.fluid.get() != nullptr){
+        scene.fluid->particles_position() += scene.fluid->particles_velocity()*dt;
+    }
 }
 
 } // Physics
